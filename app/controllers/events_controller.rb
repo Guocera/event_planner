e4b6f85f34e6a35b1d2e33a91374268c55182ae3
Nick Yee <njsyee@gmail.com>
2016-03-08 12:31:33 -0700

    Can add new tasks

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   def show
     @event = Event.find(1)
+    @task = Organizer.first.tasks.new
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,33 @@
 class TasksController < ApplicationController
+  def create
+    @organizer = Organizer.first
+    @event = Event.find(1)
+    @task = @organizer.tasks.new(task_params)
+
+    if @task.save
+      @task.update(
+        start_date: Date.today,
+        event: @event,
+        title: "#{@event.name} - #{@organizer.first_name}: #{@task.end_date}"
+      )
+      redirect_to event_path(@event)
+    else
+      redirect_to event_path(@event)
+    end
+  end
+
+
   def index
     @tasks = Organizer.first.tasks
   end
+
+  protected
+
+  def task_params
+    params.require(:task).permit(
+      :end_date,
+      :description
+    )
+  end
+
 end
