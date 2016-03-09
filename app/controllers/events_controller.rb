dc37a0a1430ae1c9d21f6bb1acfd6a1888d6f17c
Nick Yee <njsyee@gmail.com>
2016-03-08 17:41:28 -0700

    Added glyphicon for error msg

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,6 @@ class EventsController < ApplicationController
     @event = Event.find(1)
     @organizer = Organizer.first
     @task = Task.new
-    @tasks = @organizer.tasks
+    @tasks = @organizer.tasks.order("end_date DESC")
   end
 end
